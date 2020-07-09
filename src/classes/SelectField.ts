378d9c706210ccdf11240be234f7713edb1bc6bb
maciejbandyk <maciejbandyk@gmail.com>
2020-07-09 12:33:36 +0200

    Update SelectField.ts

--- src/classes/SelectField.ts
+++ src/classes/SelectField.ts
@@ -4,24 +4,30 @@ import { FieldLabel } from './FieldLabel';
 
 export class SelectField  implements IField {
 
-    constructor(name: string, label: string, value: string | null) {
-        
+    constructor(name: string, label: string, options: string[], value: string | null) {
         this.Name = name;
         this.Label = label;
+        this.Options = options;
         this.Value = value;
     }
     
     Name: string;    
     Label: string;
+    Options: string[];
     Type: FieldType = FieldType.Selectbox;
     Value: string;
-    Input: HTMLInputElement;
+    Input: HTMLSelectElement;
     
     render(container: HTMLElement): void {
         new FieldLabel(container, 'question-label', this.Label);
-        const element = document.createElement('input');
-        element.setAttribute('type', this.Type);
+        const element = document.createElement('select');
         container.appendChild(element);
+        this.Options.forEach( (text) => {
+            const optionElement = document.createElement('option');
+            optionElement.value = text;
+            optionElement.innerHTML = text;
+            element.appendChild(optionElement);
+        })
         element.value = this.Value;
         this.Input = element;
         element.addEventListener('change', this.setValue);
